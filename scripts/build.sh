3cca254f5b6d7931d9db05ac22cd6d2dbeb29730
onurcalisir27 <onur.calisir@columbia.edu>
2025-07-23 13:09:32 -0400

    Testing autonomous control

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 source /opt/ros/humble/setup.bash
 
-colcon build --cmake-clean-cache --symlink-install
-echo "Build Succesful"
+cd /$HOME/LearningAdaptive
 
-source install/setup.bash
+if [ "$#" -eq 0 ]; then
+	colcon build --cmake-clean-cache --symlink-install
+	echo "Built All Packages in Workspace"
+else
+	colcon build --cmake-clean-cache --symlink-install --packages-select "$@"
+	echo "Built the following packages: "$@""
+fi
+
+source /$HOME/LearningAdaptive/install/setup.bash
