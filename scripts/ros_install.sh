654a940e9bc2c05ed77e804dbc7451f7586e9067
onurcalisir27 <onur.calisir@columbia.edu>
2025-07-24 12:57:08 -0400

    fixing install script

--- scripts/ros_install.sh
+++ scripts/ros_install.sh
@@ -88,6 +88,7 @@ esac
 # installing pigpio
 mkdir -p /$HOME/tmp
 cd /$HOME/tmp
+sudo apt install -y python-setuptools python3-setuptools
 wget https://github.com/joan2937/pigpio/archive/master.zip
 unzip master.zip
 cd pigpio-master
