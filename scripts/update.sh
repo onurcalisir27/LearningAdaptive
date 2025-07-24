044281e00105487334537c25cf226fbe25f6dcea
Onur Calisir <onur.calisir@columbia.edu>
2025-07-24 19:28:45 -0400

    update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 REPO_PATH=$(find ~ -type d -name "LearningAdaptive" -print -quit)
-
 cd "$REPO_PATH"
+
+apt-get update && apt-get upgrade
 
 git pull origin
 
+if [ ! -f /etc/ros/rosdep/sources.list.d/20-default.list ]; then
+    sudo rosdep init
+fi
 rosdep update
-
 rosdep install --from-paths src --ignore-src -r -y --rosdistro humble
 
 source /opt/ros/humble/setup.bash
