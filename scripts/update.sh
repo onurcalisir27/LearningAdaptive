87c4168139d788ff3df75d9d0d9a768758bfe85b
Onur Calisir <onur.calisir@columbia.edu>
2025-09-29 14:28:29 -0400

    Changes to the scripts

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,17 +3,13 @@
 REPO_PATH=$(find ~ -type d -name "LearningAdaptive" -print -quit)
 cd "$REPO_PATH"
 
-apt-get update && apt-get upgrade
-
+sudo apt update && apt upgrade
 git pull origin
-
 if [ ! -f /etc/ros/rosdep/sources.list.d/20-default.list ]; then
     sudo rosdep init
 fi
 rosdep update
 rosdep install --from-paths src --ignore-src -r -y --rosdistro humble
-
 source /opt/ros/humble/setup.bash
-colcon build --cmake-clean-cache --packages-select rover_sim rover_control
-
+colcon build --cmake-clean-cache
 echo "Your repository is up to date"
